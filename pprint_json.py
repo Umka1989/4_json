26ea7163b5cdf06d6adced7b701803059578936b
Pavel Piskunov <pspiskunov@gmail.com>
2018-02-19 22:43:50 +0300

    скорректирован код. добавлена возможность запуска из командной строки. исправлена ошибка необъявления переменной

--- pprint_json.py
+++ pprint_json.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import os.path
 
 
 def load_data(filepath):
@@ -7,10 +9,14 @@ def load_data(filepath):
 
 
 def pretty_print_json(json_data):
-    print(json.dumps(json_data, sort_keys=True, indent=4,ensure_ascii=False))
+    print(json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    loaded_json = load_data(filepath)
-    pretty_print_json(loaded_json)
+    if len(sys.argv) > 1:
+        if os.path.exists(sys.argv[1]):
+            data = load_data(sys.argv[1])
+            pretty_print_json(data)
+        else:
+            print ('need positional first argument - correct path to file')
 
